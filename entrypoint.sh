99dc23961495d39016e616c8ee661c91053e1587
Sid-Ali Hamdane <93407317+sidalihmdn@users.noreply.github.com>
2025-01-09 14:49:32 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
-
+INPUT_KEEP_REPORTS=12
 mkdir -p ./${INPUT_GH_PAGES}
 mkdir -p ./${INPUT_ALLURE_HISTORY}
 
@@ -42,7 +42,7 @@ echo "if ${COUNT} > ${INPUT_KEEP_REPORTS}"
 if (( COUNT > INPUT_KEEP_REPORTS )); then
   cd ./${INPUT_ALLURE_HISTORY}
   echo "remove index.html last-history"
-  rm index.html last-history -rv
+  rm -rf index.html last-history
   echo "remove old reports"
   ls | sort -n | grep -v 'CNAME' | head -n -$((${INPUT_KEEP_REPORTS}-2)) | xargs rm -rv;
   cd ${GITHUB_WORKSPACE}
